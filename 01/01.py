f2e256b43cbc9c4bef47d7b30b726db6ab016e65
Christian Hillenbrand <cc-hillenbrand@gmx.de>
2022-12-01 22:50:09 +0100

    solve day 1

--- 01/01.py
+++ 01/01.py
@@ -1,2 +1,7 @@
 if __name__ == "__main__":
-    pass
+    with open("01") as f:
+        elves = f.read().split("\n\n")
+    
+    calories = sorted(sum(map(int, e.strip().split("\n"))) for e in elves)
+    print("Part 1:", calories[-1])
+    print("Part 2:", sum(calories[-3:]))
